d339d76c7ce3f6d63b4b65cdcf611a0f6c229426
Siddhant <me@madebysid.com>
2021-08-15 19:41:32 +0200

    feat: add demo

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,29 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 import * as path from "path";
 import { defineConfig } from "vite";
 
+const isDemo = process.env.BUILD_AS_DEMO === "true";
+
 export default defineConfig({
   plugins: [reactRefresh()],
   build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/lib.tsx"),
-      name: "useTheme",
-      formats: ["cjs", "es", "umd", "iife"],
-      fileName: (format) => `lib.${format}.js`,
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
+    emptyOutDir: true,
+    lib: isDemo
+      ? undefined
+      : {
+          entry: path.resolve(__dirname, "src/lib.tsx"),
+          name: "useTheme",
+          formats: ["cjs", "es", "umd", "iife"],
+          fileName: (format) => `lib.${format}.js`,
         },
-      },
-    },
+    rollupOptions: isDemo
+      ? {}
+      : {
+          external: ["react", "react-dom"],
+          output: {
+            globals: {
+              react: "React",
+            },
+          },
+        },
   },
 });
